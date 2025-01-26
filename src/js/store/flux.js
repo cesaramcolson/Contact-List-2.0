3da132277b3a87d2f14ecc23e366bd7f859916e5
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2025-01-26 15:53:08 +0000

    working in the flux to retrive data from the API, create user, check if user exist and fetch contacts were created

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,67 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			contacts: [],
+			baseUrl: 'https://playground.4geeks.com/contact/agendas/cesar-amcolson'
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			userExists: async () => {
+				try {
+					const response = await fetch(`${store.baseUrl}`, {
+						method: "GET",
+						headers: {
+							'Content-Type': 'application/json'
+						}
+					});
+					if (response.status === 404) {
+						return false;
+					}
+					return response.ok
+				} catch (error) {
+					console.error("Error Checking User Existance: ", error)
+					return false;
+				}
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			createUser: async () => {
+				try {
+					const response = await fetch(`${store.baseUrl}`, {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json"
+						}
+					});
+					if(!response.ok) {
+						throw new Error(`error status: ${response.status}`);
+					}
+					const data = await response.json();
+					console.log("User created successfully:", data);
+				} catch (error) {
+					console.error("Error Creating the User: ", error)
+				}
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			fetchContacts: async () => {
+				const actions = getActions()
+				try {
+					const exist = await actions.userExists();
+					if (!exist) {
+						await actions.createUser();
+					}
+					const response = await fetch(`${store.baseUrl}/contacts`, {
+						method: "GET",
+						headers: {
+							"Content-Type": "application/json"
+						}
+					});
+					if (!response.ok) {
+						throw new Error(`Error Status: ${response.status}`);
+					}
+					let data = await response.json()
+					console.log(data)
+					setStore({ contacts: data.contacts });
+					return getStore.contacts();
+				} catch(error) {
+					console.error("Error Fetching Contacts: ", error)
+				}
 			}
 		}
 	};
